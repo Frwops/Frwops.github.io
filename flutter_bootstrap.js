a0bba02c098d460f53dc74eb24d5085105fec6a6
Francisco Soto Reyes <104473569+Frwops@users.noreply.github.com>
2024-08-21 15:40:07 -0400

    Add files via upload
    Proyecto completo, listo para actualizarse

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1792240761"
+    serviceWorkerVersion: "3293784450"
   }
 });
